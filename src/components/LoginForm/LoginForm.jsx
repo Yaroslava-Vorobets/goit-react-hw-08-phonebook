5e7f78a56606ef6402bad450924f3736bf010056
Yaroslava-Vorobets <slavavor85@gmail.com>
2025-01-01 00:14:41 +0100

    change url

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
 import {
-  FormControl,
-  FormLabel, 
+  Box,
+  Text,
   Input,
-  FormHelperText,
-  Button
-} from '@chakra-ui/react'
+  Button,
+} from '@chakra-ui/react';
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
@@ -20,26 +19,85 @@ export const LoginForm = () => {
         password: form.elements.password.value,
       })
     );
-    console.log(logIn)
+    console.log(logIn);
     form.reset();
   };
 
   return (
     <form onSubmit={handleSubmit} autoComplete="off">
-       <FormControl>
-        <FormLabel fontSize={25} color='#805AD5'>Email</FormLabel>
-        <Input border= '2px solid' borderColor = '#D53F8C' type="email" name="email" placeholder='Email'  _hover={{ borderColor: '#FED7E2' }}  _focusVisible = 'none'
-       
-   />      
-      <FormHelperText>
-        Enter the email you'd like to receive the newsletter on.
-      </FormHelperText>    
-      </FormControl>  
-       <FormControl >
-        <FormLabel fontSize={25} color='#805AD5'> Password</FormLabel>
-        <Input  border= '2px solid' borderColor = '#D53F8C' type="password" name="password" placeholder=' Password'  _hover={{ borderColor: '#FED7E2' }} _focusVisible = 'none'/>
-      </FormControl> 
-      <Button  mt = {6} type="submit" colorScheme='pink'>Log In</Button>
+      <Box mb={4}>
+        <Text fontSize={25} color='#805AD5'>Email</Text>
+        <Input
+          border='2px solid'
+          borderColor='#D53F8C'
+          type="email"
+          name="email"
+          placeholder='Email'
+          _hover={{ borderColor: '#FED7E2' }}
+          _focusVisible={{ borderColor: '#D53F8C' }} // Задайте стиль при фокусі
+        />
+        <Text>
+          Enter the email you'd like to receive the newsletter on.
+        </Text>
+      </Box>
+      <Box mb={4}>
+        <Text fontSize={25} color='#805AD5'>Password</Text>
+        <Input
+          border='2px solid'
+          borderColor='#D53F8C'
+          type="password"
+          name="password"
+          placeholder='Password'
+          _hover={{ borderColor: '#FED7E2' }}
+          _focusVisible={{ borderColor: '#D53F8C' }} // Задайте стиль при фокусі
+        />
+      </Box>
+      <Button mt={6} type="submit" colorScheme='pink'>Log In</Button>
     </form>
   );
 };
+// import { useDispatch } from 'react-redux';
+// import { logIn } from 'redux/auth/operations';
+// import {
+//   FormControl,
+//   FormLabel, 
+//   Input,
+//   FormHelperText,
+//   Button
+// } from '@chakra-ui/react'
+
+// export const LoginForm = () => {
+//   const dispatch = useDispatch();
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     const form = e.currentTarget;
+//     dispatch(
+//       logIn({
+//         email: form.elements.email.value,
+//         password: form.elements.password.value,
+//       })
+//     );
+//     console.log(logIn)
+//     form.reset();
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit} autoComplete="off">
+//        <FormControl>
+//         <FormLabel fontSize={25} color='#805AD5'>Email</FormLabel>
+//         <Input border= '2px solid' borderColor = '#D53F8C' type="email" name="email" placeholder='Email'  _hover={{ borderColor: '#FED7E2' }}  _focusVisible = 'none'
+       
+//    />      
+//       <FormHelperText>
+//         Enter the email you'd like to receive the newsletter on.
+//       </FormHelperText>    
+//       </FormControl>  
+//        <FormControl >
+//         <FormLabel fontSize={25} color='#805AD5'> Password</FormLabel>
+//         <Input  border= '2px solid' borderColor = '#D53F8C' type="password" name="password" placeholder=' Password'  _hover={{ borderColor: '#FED7E2' }} _focusVisible = 'none'/>
+//       </FormControl> 
+//       <Button  mt = {6} type="submit" colorScheme='pink'>Log In</Button>
+//     </form>
+//   );
+// };
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,13 +1,73 @@
+// import { useDispatch } from 'react-redux';
+// import { logIn } from 'redux/auth/operations';
+// import {
+//   Box,
+//   Text,
+//   Input,
+//   FormHelperText,
+//   Button,
+// } from '@chakra-ui/react';
+
+// export const LoginForm = () => {
+//   const dispatch = useDispatch();
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     const form = e.currentTarget;
+//     dispatch(
+//       logIn({
+//         email: form.elements.email.value,
+//         password: form.elements.password.value,
+//       })
+//     );
+//     console.log(logIn);
+//     form.reset();
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit} autoComplete="off">
+//       <Box mb={4}>
+//         <Text fontSize={25} color='#805AD5'>Email</Text>
+//         <Input
+//           border='2px solid'
+//           borderColor='#D53F8C'
+//           type="email"
+//           name="email"
+//           placeholder='Email'
+//           _hover={{ borderColor: '#FED7E2' }}
+//           _focusVisible={{ borderColor: '#D53F8C' }} // Задайте стиль при фокусі
+//         />
+//         <FormHelperText>
+//           Enter the email you'd like to receive the newsletter on.
+//         </FormHelperText>
+//       </Box>
+//       <Box mb={4}>
+//         <Text fontSize={25} color='#805AD5'>Password</Text>
+//         <Input
+//           border='2px solid'
+//           borderColor='#D53F8C'
+//           type="password"
+//           name="password"
+//           placeholder='Password'
+//           _hover={{ borderColor: '#FED7E2' }}
+//           _focusVisible={{ borderColor: '#D53F8C' }} // Задайте стиль при фокусі
+//         />
+//       </Box>
+//       <Button mt={6} type="submit" colorScheme='pink'>Log In</Button>
+//     </form>
+//   );
+// };
+
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
 
 
 import {
-  FormControl,
-  FormLabel, 
-  Input,
-  FormHelperText,
+  Box, 
+  Text, 
+  Input, 
   Button
+
 } from '@chakra-ui/react'
 
 
@@ -33,22 +93,41 @@ export const RegisterForm = () => {
   return (
 
     <form onSubmit={handleSubmit} autoComplete="off">
-      <FormControl>
-        <FormLabel fontSize={25} color='#805AD5'>Username</FormLabel>
-        <Input border= '2px solid' borderColor = '#D53F8C'  mb={4} type="text" name="name" placeholder='Username'  _focusVisible={"none"} />      
-      </FormControl> 
-       <FormControl>
-        <FormLabel fontSize={25} color='#805AD5'>Email</FormLabel>
-        <Input border= '2px solid' borderColor = '#D53F8C' type="email" name="email" placeholder='Email'  _hover={{ borderColor: '#FED7E2' }}  _focusVisible={"none"} />      
-      <FormHelperText>
+      <Box mb={4}>    
+        <Text fontSize={25} color='#805AD5'>Username</Text>
+        <Input
+          border='2px solid'
+          borderColor='#D53F8C'
+          mb={4}
+          type="text"
+          name="name"
+          placeholder='Username'
+          _focusVisible={{ borderColor: '#D53F8C' }} />       
+        <Text fontSize={25} color='#805AD5'>Email</Text>
+        <Input
+          border='2px solid'
+          borderColor='#D53F8C'
+          type="email" name="email"
+          placeholder='Email'
+          _hover={{ borderColor: '#FED7E2' }}
+          _focusVisible={{ borderColor: '#D53F8C' }} />      
+      <Text>
         Enter the email you'd like to receive the newsletter on.
-      </FormHelperText>    
-      </FormControl>   
-      <FormControl >
-        <FormLabel fontSize={25} color='#805AD5'> Password</FormLabel>
-        <Input  border= '2px solid' borderColor = '#D53F8C' type="password" name="password" placeholder='Password must consist of seven characters'  _hover={{ borderColor: '#FED7E2' }}  _focusVisible={"none"}/>
-      </FormControl>  
-      <Button  mt = {6} type="submit" colorScheme='pink'>Register</Button>
+      </Text>     
+    </Box>
+    <Box>
+      <Text fontSize={25} color='#805AD5'> Password</Text>
+      <Input
+        border='2px solid'
+        borderColor='#D53F8C'
+        type="password"
+        name="password"
+        placeholder='Password must consist of seven characters'
+        _hover={{ borderColor: '#FED7E2' }}
+        _focusVisible={{ borderColor: '#D53F8C' }} />
+      
+      <Button mt={6} type="submit" colorScheme='pink'>Register</Button>
+    </Box>
     </form>
   );
 };
